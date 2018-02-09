2c5e7c5552932fbb938dd22d53cb5ee7896e579f
Rocia <rocia.fernandes@gmail.com>
2018-02-09 17:40:21 +0530

    Update

--- 1_Intro.py
+++ 1_Intro.py
@@ -99,18 +99,22 @@ def game_intro():
         clock.tick(15)
         
     
- def button(msg,x,y,w,h,ic,ac):
+def button(msg,x,y,w,h,ic,ac,action=None):
     mouse = pygame.mouse.get_pos()
-
+    click = pygame.mouse.get_pressed()
+    print(click)
     if x+w > mouse[0] > x and y+h > mouse[1] > y:
         pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+
+        if click[0] == 1 and action != None:
+            action()         
     else:
         pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
 
-    smallText = pygame.font.Font("freesansbold.ttf",20)
+    smallText = pygame.font.SysFont("comicsansms",20)
     textSurf, textRect = text_objects(msg, smallText)
     textRect.center = ( (x+(w/2)), (y+(h/2)) )
-    gameDisplay.blit(textSurf, textRect)   
+    gameDisplay.blit(textSurf, textRect)
 
     
 def game_loop():
