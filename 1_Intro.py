661bcbf720ce18fe30c142078825f98546de9ea2
Rocia <rocia.fernandes@gmail.com>
2018-02-09 17:38:37 +0530

    Update

--- 1_Intro.py
+++ 1_Intro.py
@@ -54,13 +54,19 @@ def message_display(text):
 def crash():
     message_display('You Crashed')
 
+red = (200,0,0)
+green = (0,200,0)
+
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+
 def game_intro():
 
     intro = True
 
     while intro:
         for event in pygame.event.get():
-            print(event)
+            #print(event)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
@@ -71,13 +77,18 @@ def game_intro():
         TextRect.center = ((display_width/2),(display_height/2))
         gameDisplay.blit(TextSurf, TextRect)
 
-        pygame.draw.rect(gameDisplay, green,(150,450,100,50))
-        pygame.draw.rect(gameDisplay, 'red',(550,450,100,50))
 
+        mouse = pygame.mouse.get_pos()
 
+        #print(mouse)
+
+        if 150+100 > mouse[0] > 150 and 450+50 > mouse[1] > 450:
+            pygame.draw.rect(gameDisplay, bright_green,(150,450,100,50))
+        else:
+            pygame.draw.rect(gameDisplay, green,(150,450,100,50))
+        pygame.draw.rect(gameDisplay, red,(550,450,100,50))
         pygame.display.update()
         clock.tick(15)
-        
         
     
     
